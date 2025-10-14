c39e54b91bfdf01b9468bc6dd412d7db6bdf69b0
Dewiauliarizki <auliariskidewi@gmail.com>
2025-10-14 10:07:06 +0700

    Create metodeget.php

---
+++ p2/Latihan p2/metodeget.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Form Method GET</title>
+</head>
+<body>
+    <h1>Input</h1>
+    <form action="methodgetproses.php" method="get">
+        Masukkan nama: 
+        <input type="text" name="nama" size="25">
+        <br><br>
+        <input type="submit" value="Proses">
+    </form>
+</body>
+</html>
+
