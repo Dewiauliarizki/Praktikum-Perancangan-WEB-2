88923041a15ea333f54181c140e5bd6223267131
Dewiauliarizki <auliariskidewi@gmail.com>
2025-10-14 21:02:12 +0700

    Add files via upload

---
+++ P1/Latihan/variabel.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEAD>
+        <TITLE>P1</TITLE>
+    </HEAD>
+        <BODY>
+            <?php
+            $nama = "Dewi Aulia Rizki";                         
+            echo "$nama berkata \"Katakan namamu!\"";
+            ?>
+        </BODY>
+</HTML>                                                     
